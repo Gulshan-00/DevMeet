207a2f8d6467f1094374432446847ffca48dd335
Gulshan-00 <rr3870044@gmail.com>
2024-12-01 20:56:43 +0530

    Schema Validations

--- src/app.js
+++ src/app.js
@@ -10,14 +10,11 @@ app.get("/admin/data",(req,res)=>{
     res.send("Data accessed");
 })
 
+app.use(express.json());
+
 app.post("/signup",async(req,res)=>{
-    const userObj={
-    firstName:"unnati",
-    lastName:"chauhan",
-    emailId:"unnati12@gmail.com",
-    password:"Unn@123",
-    gender:"Female",
-    }
+
+    const userObj=(req.body)
 
     const user=new User(userObj);
     try{
@@ -27,6 +24,61 @@ app.post("/signup",async(req,res)=>{
         res.status(400).send("Error saving the user" + err.message);
     }
 })
+
+
+//All the user in data will be displayed
+app.get("/feed",async (req,res)=>{
+    const users=await User.find({})
+    // console.log(users);
+    try {
+        res.send(users);
+    } catch (error) {
+        res.status(401).send("Something went wrong",+error.message);
+    }
+    
+})
+
+//show according to the userID 
+app.get("/feed/:userId",async (req,res)=>{
+    const id=req.params.userId;
+    const idfind=await User.findById(id)
+   
+    try {
+        res.send(idfind);
+    } catch (error) {
+        res.status(401).send("Something went wrong",+error.message);
+    }
+})
+//to delete the user data
+app.delete("/delete",async (req,res)=>{
+    const userid=req.body.userId;
+
+    try{
+          const userDelete=await User.findByIdAndDelete(userid);
+        res.send("Yes user is successfully deleted");
+    }catch(error){
+      console.log("User is successfully deleted");
+    }
+    
+})
+//to update the user information using the patch
+app.patch("/user",async (req,res)=>{
+    const userid=req.body.userId;
+    const userData=req.body;
+
+    try{
+      await User.findByIdAndUpdate({_id:userid},userData,{
+        returnDocument:"after",
+        runValidators:true,
+      });
+      res.send("User is successfully updated");
+
+    }catch(err){
+        res.status(400).send("Something went wrong"+err.message);
+    }
+})
+
+
 
 
 connectDB().then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -3,19 +3,49 @@ const mongoose= require("mongoose");
 const userSchema=new mongoose.Schema({
     firstName:{
         type: String,
+        required:true,
+        minLength:5,
     },
     lastName:{
         type: String,
+        
     },
     emailId:{
         type:String,
+        required:true,
+        trim: true,
+        unique: true,
+        lowercase:true,
     },
     password:{
         type:String,
+        required:true,
+        minLength:8,
+        maxLength:20,
     },
     gender:{
         type:String,
+        required:true,
+        validate(value){
+                if(!["male","female","others"].includes(value)){
+                    throw new Error("Gender data is not valid");
+                }
+        },
+    },
+    photoUrl:{
+        type:String,
+        default:"https//grographyandyou.com/images/user-profile.png",
+    },
+    about:{
+        type:String,
+        default:"This is the default about section",
+    },
+    skills:{
+        type:[String],
+        required:true,
     }
+},{
+    timestamps:true,
 })
 
 const UserModel= mongoose.model("User",userSchema);
